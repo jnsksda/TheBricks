15dec1ea8d79eabf7001aea02a3e94a2762d7ed6
jnsksda <97179902+jnsksda@users.noreply.github.com>
2022-04-14 19:07:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var x = 150;
-var y = 150;//a
-var dx = 2;
-var dy = 4;
+var x = 350;
+var y = 580;
+var dx = 0;
+var dy = -2;
 var WIDTH;
 var HEIGHT;
-var r = 10;
+var r = 20;
 var ctx;
 var spc=1;
 var paddlex;
@@ -33,9 +33,7 @@ var gejmover = true;
 var timerTemp = 1;
 var gameWin=false;
 var isClicked = false;
-var brick = new Image();
-brick.src = "img/brick.png";
-var img = document.getElementById("brick");
+
 
 
 
@@ -46,70 +44,8 @@ function drawIt() {
     document.getElementById("start").innerHTML = "Start";
   } 
   
-  //timer
-  function timer(){
-sekunde++;
-
-sekundeI = ((sekundeI = (sekunde % 60)) > 9) ? sekundeI : "0"+sekundeI;
-minuteI = ((minuteI = Math.floor(sekunde / 60)) > 9) ? minuteI : "0"+minuteI;
-izpisTimer = minuteI + ":" + sekundeI;
-$("#cas").html("Time:<br/>" + izpisTimer);
-}
-
-/////tipkovnica
-  function onKeyDown(evt) {
-    if (evt.keyCode == 39 || evt.keyCode == 68) rightDown = true;
-    else if (evt.keyCode == 37 || evt.keyCode == 65) leftDown = true;
-  }
-
-  function onKeyUp(evt) {
-    if (evt.keyCode == 39 || evt.keyCode == 68) rightDown = false;
-    else if (evt.keyCode == 37 || evt.keyCode == 65) leftDown = false;
-  }
-  function onKeyPress(evt) {
-    if (evt.keyCode == 80) pDown = true;
-    console.log(pDown);
-  }
-  $(document).keypress(onKeyPress);
-  $(document).keydown(onKeyDown);
-  $(document).keyup(onKeyUp);
-
   
-  function initbricks() {
-    //inicializacija opek
-    NROWS = 5;
-    NCOLS = 7;
-    BRICKWIDTH = WIDTH / NCOLS - 1;
-    BRICKHEIGHT = 15;
-    PADDING = 2;
-    bricks = new Array(NROWS);
-    for (i = 0; i < NROWS; i++) {
-      bricks[i] = new Array(NCOLS);
-      for (j = 0; j < NCOLS; j++) {
-        bricks[i][j] = 1;
-      }
-    }
-  }
-  function init_paddle() {
-    paddlex = WIDTH / 2;
-    paddleh = 7;
-    paddlew = 100;
-  }
-
-  function init() {
-	canvasMinX = $("canvas").offset().left;
-    canvasMaxX = canvasMinX + WIDTH;
-    sekunde = 0;
-    izpisTimer = "00:00";
-    intTimer = setInterval(timer, 1000);
-    tocke = 0;
-    $("#tocke").html(tocke);
-    ctx = $("#canvas")[0].getContext("2d");
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-    return setInterval(draw, 7);
-  }
-
+   //izdelava zogice
   function circle(x, y, r) {
     ctx.fillStyle = temp;
     ctx.beginPath();
@@ -117,21 +53,16 @@ $("#cas").html("Time:<br/>" + izpisTimer);
     ctx.closePath();
     ctx.fill();
   }
-
-  function rect(x, y, w, h) {
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  }
+  
+  
+  
+  
+  
   function draw() {
     clear();
 
-    circle(x, y, 10);
+    circle(x, y, r/2);
+	rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
     //omejevanje ploscka, pomik
     if (rightDown) {
       if (paddlex + paddlew < WIDTH) {
@@ -147,16 +78,15 @@ $("#cas").html("Time:<br/>" + izpisTimer);
       }
     }
 
-    rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
-
+    
+	////risanje opek
     for (i = 0; i < NROWS; i++) {
       for (j = 0; j < NCOLS; j++) {
         if (bricks[i][j] == 1) {
           rect(
             j * (BRICKWIDTH + PADDING) + PADDING,
             i * (BRICKHEIGHT + PADDING) + PADDING,
-            BRICKWIDTH,
-            BRICKHEIGHT
+            BRICKWIDTH, BRICKHEIGHT
           );
         }
       }
@@ -182,6 +112,12 @@ $("#cas").html("Time:<br/>" + izpisTimer);
       temp = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
     }
 
+
+
+
+
+//////////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     if (x + dx > WIDTH - r || x + dx < r) {
       dx = -dx;
     }
@@ -190,15 +126,14 @@ $("#cas").html("Time:<br/>" + izpisTimer);
     } else if (y + dy > HEIGHT - r) {
 		//odboj
       if (x > paddlex && x < paddlex + paddlew) {
-        dx = 8 * ((x - (paddlex + paddlew / 2)) / paddlew);
+        dx = 7 * ((x - (paddlex + paddlew / 2)) / paddlew);
         dy = -dy;
         //start=true;
        
        
       } else {
         start = false;
-        //clearInterval(IntervalId);
-        //console.log("Game Over");
+       
         gejmover = false;
       }
     }
@@ -229,13 +164,103 @@ $("#cas").html("Time:<br/>" + izpisTimer);
     }
       
   }
+  
+  
+  
+  function init() {
+    canvasMinX = $("canvas").offset().left;
+	canvasMaxX = canvasMinX + WIDTH;
+	sekunde = 0;
+	izpisTimer = "00:00";
+	intTimer = setInterval(timer, 1000);
+	tocke = 0;
+	$("#tocke").html(tocke);
+	ctx = $("#canvas")[0].getContext("2d");
+	WIDTH = $("#canvas").width();
+	HEIGHT = $("#canvas").height();
+	return setInterval(draw, 10);
+  }
+  
+  
+  
+  
+  
+  
+  //timer
+  function timer(){
+sekunde++;
+
+sekundeI = ((sekundeI = (sekunde % 60)) > 9) ? sekundeI : "0"+sekundeI;
+minuteI = ((minuteI = Math.floor(sekunde / 60)) > 9) ? minuteI : "0"+minuteI;
+izpisTimer = minuteI + ":" + sekundeI;
+$("#cas").html("Time:<br/>" + izpisTimer);
+}
+
+/////tipkovnica
+  function onKeyDown(evt) {
+    if (evt.keyCode == 39 || evt.keyCode == 68) rightDown = true;
+    else if (evt.keyCode == 37 || evt.keyCode == 65) leftDown = true;
+  }
+
+  function onKeyUp(evt) {
+    if (evt.keyCode == 39 || evt.keyCode == 68) rightDown = false;
+    else if (evt.keyCode == 37 || evt.keyCode == 65) leftDown = false;
+  }
+  function onKeyPress(evt) {
+    if (evt.keyCode == 80) pDown = true;
+    console.log(pDown);
+  }
+  $(document).keypress(onKeyPress);
+  $(document).keydown(onKeyDown);
+  $(document).keyup(onKeyUp);
+
+
+
+
+  //inicializacija opek
+  function initbricks() {
+    NROWS = 5;
+    NCOLS = 7;
+    BRICKWIDTH = WIDTH / NCOLS - 1;
+    BRICKHEIGHT = 60;
+    PADDING = 2;
+    bricks = new Array(NROWS);
+    for (i = 0; i < NROWS; i++) {
+      bricks[i] = new Array(NCOLS);
+      for (j = 0; j < NCOLS; j++) {
+        bricks[i][j] = 1;
+      }
+    }
+  }
+  //inicializacija ploscka
+  function init_paddle() {
+    paddlex = WIDTH / 2;
+	/////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    paddleh = 10;
+    paddlew = 100;
+  }
+
+  
+ 
+
+  function rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  }
+
 
   function gameover() {
     clear();
     ctx.fillStyle="black";
-    ctx.font = "bold 72px sans-serif";
+    ctx.font = "bold 70px sans-serif";
     ctx.fillText("Game Over", WIDTH / 2 - 200, HEIGHT / 2);
-    ctx.font = "bold 36px sans-serif";
+    ctx.font = "bold 35px sans-serif";
     ctx.fillText("Score: " + tocke, WIDTH / 2 - 200, HEIGHT / 2 + 100);
     ctx.fillText("Time: " +izpisTimer, WIDTH / 2 - 200, HEIGHT / 2 + 150);
     start = true;
@@ -248,9 +273,9 @@ $("#cas").html("Time:<br/>" + izpisTimer);
   function gamewon() {
     clear();
     ctx.fillStyle="black";
-    ctx.font = "bold 72px sans-serif";
+    ctx.font = "bold 70px sans-serif";
     ctx.fillText("You Won!", WIDTH / 2 - 200, HEIGHT / 2);  
-    ctx.font = "bold 36px sans-serif";
+    ctx.font = "bold 35px sans-serif";
     ctx.fillText("Score: " + tocke, WIDTH / 2 - 200, HEIGHT / 2 + 100);
     ctx.fillText("Time: " +izpisTimer, WIDTH / 2 - 200, HEIGHT / 2 + 150);
     start = true;
